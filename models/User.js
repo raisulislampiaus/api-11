0442840f61bfd7cb4ae7b99f29db2b0c2140fdc0
Raisul islam piaus <raisulislam714@gmail.com>
2023-02-05 00:16:53 +0600

    nploy44 commit

---
+++ models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const itemsSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  phone: { type: Number, required: true },
+  address: { type: String, required: true },
+  division: {
+    type: String, required: true
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  },
+  image: { type: String, required: true },
+}, { timestamps: true });
+
+const itemsModel = mongoose.model("users", itemsSchema);
+
+module.exports = itemsModel;
